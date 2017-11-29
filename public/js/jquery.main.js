56a40dd82c68dbc750f443f29eb93e5fc49729f6
OlegMarchenko <workolegmarchenko@gmail.com>
2017-11-29 23:28:11 +0200

    start-new-avers

---
+++ public/js/jquery.main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // cache the window object
+
+    if ($('.v-header').length) {
+        var $window = $(window);
+
+        // Parallax background effect
+
+        $('.v-header[data-type="background"]').each(function () {
+
+            var $bgobj = $(this); // assigning the object
+
+            $(window).scroll(function () {
+
+                // scroll the background at var speed
+                // The yPos is a negative value because we're scrolling iy UP!
+
+                var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+                // Put together out final background position
+                var coords = '50% ' + yPos + 'px';
+
+                // Move the background
+                $bgobj.css({backgroundPosition: coords});
+
+            }); // end window scroll
+
+        });
+    }
+
+    if($('.secondary-image').length) {
+        $('.secondary-image').slick({
+            slidesToShow: 4
+        });
+    }
+
+});
